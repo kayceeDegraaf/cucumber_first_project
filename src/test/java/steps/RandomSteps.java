2ee923360bc93110976c95668aefaef952a52b9b
kayceeDegraaf <katelyndegraaf@gmail.com>
2022-06-22 20:34:55 -0500

    June 22

---
+++ src/test/java/steps/RandomSteps.java
@@ -0,0 +1,23 @@
+package steps;
+
+import com.github.javafaker.Faker;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import utils.Driver;
+
+public class RandomSteps {
+
+    //EXAMPLE FROM CLASS
+
+    @Test
+    public void getRandomGoogleSearch(){
+        Faker faker = new Faker();
+
+        Driver.getDriver().get("http://www.google.com");
+
+        Driver.getDriver().findElement(By.name("q")).sendKeys(faker.gameOfThrones().character() + Keys.ENTER);
+
+        Driver.quitDriver();
+    }
+}
